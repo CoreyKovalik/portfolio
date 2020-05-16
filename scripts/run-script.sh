e1c590bc546e1ef5ebe91648cf63fcf484391ba6
Corey Kovalik <coreykovalik@gmail.com>
2020-05-16 09:02:24 -0700

    add some better infra for running scripts / surge

---
+++ scripts/run-script.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"
+cd ..
+
+source ./scripts/common/colors.sh
+
+if [ $# -eq 0 ]
+  then
+    echo "Run a script from root:  usage: ./scripts/run-script.sh <script-name.sh> ...args"
+    exit
+fi
+
+echo -e "${COLORS_CYAN}Running Script${COLORS_END}:  ./scripts/$1 ${@:2}"
+
+bash ./scripts/$1
--- scripts/surge.sh
+++ scripts/surge.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-cd ..
-
-node ./node_modules/surge/lib/cli.js
+node ./node_modules/surge/lib/cli.js $@
