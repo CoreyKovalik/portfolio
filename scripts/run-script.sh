76321411eab693f2544c8b177844b741218b4c0d
Corey Kovalik <coreykovalik@gmail.com>
2020-05-16 10:57:55 -0700

    many changes to support deploying via surge

--- scripts/run-script.sh
+++ scripts/run-script.sh
@@ -5,12 +5,11 @@ cd ..
 
 source ./scripts/common/colors.sh
 
-if [ $# -eq 0 ]
-  then
+if [ $# -eq 0 ]; then
     echo "Run a script from root:  usage: ./scripts/run-script.sh <script-name.sh> ...args"
     exit
 fi
 
 echo -e "${COLORS_CYAN}Running Script${COLORS_END}:  ./scripts/$1 ${@:2}"
 
-bash ./scripts/$1
+bash ./scripts/$1 ${@:2}
--- scripts/surge.sh
+++ scripts/surge.sh
@@ -1,1 +1,5 @@
-node ./node_modules/surge/lib/cli.js $@
+if [ "$1" == "help" ]; then
+    node ./node_modules/surge/lib/cli.js --help
+else
+    node ./node_modules/surge/lib/cli.js $@
+fi
